dbfd481cb8121f15782537295bbbfc121d7b70d0
unknown <thomas.feraud@moona.com>
2022-12-19 13:29:49 +0100

    firewall rule to allow azure services connectivity to db server

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,9 +16,18 @@ resource "azurerm_mysql_server" "mysql_server_wp" {
   storage_mb = var.mysql_storage
   version    = var.mysql_version
 
-  backup_retention_days        = 7
-  geo_redundant_backup_enabled = false
-  ssl_enforcement_enabled      = true
+  backup_retention_days            = 7
+  geo_redundant_backup_enabled     = false
+  ssl_enforcement_enabled          = false
+  ssl_minimal_tls_version_enforced = "TLSEnforcementDisabled"
+}
+
+resource "azurerm_mysql_firewall_rule" "mysql_allow_azure" {
+  name                = "allowAzureIPs"
+  resource_group_name = azurerm_mysql_server.mysql_server_wp.resource_group_name
+  server_name         = azurerm_mysql_server.mysql_server_wp.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
 }
 
 # Create MySQL database
